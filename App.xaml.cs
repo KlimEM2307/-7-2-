32c9cc4a262b69313225b8d0e821ed652fea9b6e
KlimovichEM <KlimovichEM@310D-1>
2024-10-31 11:04:47 +0700

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace л7_2_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace л7_2_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const double Offset = 20;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            button.MouseEnter += Button_MouseEnter;
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var button = sender as Button;
+            if (button != null)
+            {
+                var mousePosition = e.GetPosition(this);
+
+                button.Margin = new Thickness(
+                    mousePosition.X + Offset,
+                    mousePosition.Y + Offset,
+                    0,
+                    0
+                );
+            }
+        }
+    }
+}
